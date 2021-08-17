b7c60ae93cd41cda988ce8dd22f88cc06cae141e
Jessa Mae Alcantara <the.epikskeptic@gmail.com>
2021-08-17 19:29:04 -0400

    Updated more from tutorial

---
+++ routes/author.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+const Author = require('../models/author')
+
+//All Authors Route
+router.get('/', (req, res) => {
+    res.render('authors/index')
+})
+
+// Create Author Route
+router.post('/', (req, res) => {
+    res.send(req.body.name)
+})
+
+router.get('/new', (req, res) => {
+    res.render('authors/new', { author: new Author()})
+})
+module.exports = router
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
+
+
+
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 const db = mongoose.connection
@@ -13,13 +16,16 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/author')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: false, limit: '10mb'}))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
