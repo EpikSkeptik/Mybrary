694759370cab2474b04df6646e888e478ad1dff8
Jessa Mae Alcantara <the.epikskeptic@gmail.com>
2021-08-18 11:03:04 -0400

    author post

--- routes/author.js
+++ routes/author.js
@@ -9,7 +9,20 @@ router.get('/', (req, res) => {
 
 // Create Author Route
 router.post('/', (req, res) => {
-    res.send(req.body.name)
+    const author = new Author({
+        name: req.body.name
+    })
+    author.save((err, newAuthor) => {
+        if (err) {
+            res.render('authors/new', {
+                author: author,
+                errorMessage: 'Error creating Author'
+            })
+        } else{
+            // res.redirect(`authors/${newAuthor.id}`)
+            res.redirect('authors')
+        }
+    })
 })
 
 router.get('/new', (req, res) => {
